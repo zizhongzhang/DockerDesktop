422302bfca7a7035e4abd92fc4e499bb41d26533
zizhong.zhang <zizhong@sudaka.com.au>
2023-02-23 20:32:40 +1100

    Verify debugging works with linux container

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 set -e
 
 dockercli -SwitchLinuxEngine
-docker run --rm -d -p 5001:80 -e ASPNETCORE_ENVIRONMENT=Development apicore/dev
+# docker build -t dev/apicore:latest -f ./API.Core/Dockerfile .
+docker run --rm -d -p 5001:80 -e ASPNETCORE_ENVIRONMENT=Development dev/apicore:latest
 
 dockercli -SwitchWindowsEngine
-docker run --rm -d -p 5000:80 webframework/dev
+docker run --rm -d -p 5000:80 dev/webframework:latest
