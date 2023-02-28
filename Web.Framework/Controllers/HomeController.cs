8b5186e2846ea953044d5784eca3e8a7f8c25fa4
zizhong zhang <zizhong.zhang@caterxpress.com>
2023-02-28 16:42:02 +1100

    Apose.Words support custom fonts

--- Web.Framework/Controllers/HomeController.cs
+++ Web.Framework/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Specialized;
 using System;
 using System.Configuration;
+using System.IO;
 using System.Net.Http;
+using System.Text;
 using System.Web.Mvc;
+using Aspose.Words.Fonts;
 
 namespace Web.Framework.Controllers
 {
@@ -28,7 +31,28 @@ namespace Web.Framework.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = ConfigurationManager.AppSettings["complex:setting2"];
+            var fontSettings = new FontSettings();
+            var sources = fontSettings.GetFontsSources();
+            var fonts = sources[0].GetAvailableFonts();
 
+            var directory = Directory.GetCurrentDirectory();
+            var files = Directory.GetFiles(directory);
+            
+            fontSettings.SetFontsFolders(new[] { "C:/Users/ZizhongZhang/Dev/DockerDesktop/Web.Framework/App_Data", "C:/Windows/Fonts" }, false);
+            
+            sources = fontSettings.GetFontsSources();
+            
+            var stringBuilder = new StringBuilder();
+            foreach (var source in sources)
+            {
+                foreach (var font in source.GetAvailableFonts())
+                {
+                    stringBuilder.Append(font.FullFontName);
+                    stringBuilder.AppendLine();
+                }
+            }
+
+            ViewBag.Message = stringBuilder.ToString();
             return View();
         }
     }
--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ dockercli -SwitchLinuxEngine
 docker run --rm -d -p 5001:80 -e ASPNETCORE_ENVIRONMENT=Development dev/apicore:latest
 
 dockercli -SwitchWindowsEngine
-# docker build -t dev/webframework:latest --build-arg source=./Web.Framework/obj/Docker/publish  --network "Default Switch" -f ./Web.Framework/Dockerfile .
+# docker build -t dev/webframework:latest --build-arg source=./Web.Framework/obj/Docker/publish -f ./Web.Framework/Dockerfile .
 docker run --rm -d -p 5000:80 dev/webframework:latest
